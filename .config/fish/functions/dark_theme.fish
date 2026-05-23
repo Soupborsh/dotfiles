44d0e6f8aba8199c10ab496cbb3d2a035fb8c56f
Soupborsh <100529201+Soupborsh@users.noreply.github.com>
2026-05-23 21:52:20 +0500

    Dark/Light add gtk support

--- .config/fish/functions/dark_theme.fish
+++ .config/fish/functions/dark_theme.fish
@@ -1,3 +1,4 @@
 function dark_theme
     ln -sf /usr/share/color-schemes/BreezeDark.colors ~/.config/qt6ct/theme_link.colors
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 end
--- .config/fish/functions/light_theme.fish
+++ .config/fish/functions/light_theme.fish
@@ -1,3 +1,4 @@
 function light_theme
     ln -sf /usr/share/color-schemes/BreezeLight.colors ~/.config/qt6ct/theme_link.colors
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 end
